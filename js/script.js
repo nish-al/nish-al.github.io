2933a21462ebf42f087e17703abb93c7d479d76b
ns2max <ns2max@gmail.com>
2024-09-25 00:43:08 +0200

    ajax mail

--- js/script.js
+++ js/script.js
@@ -10,35 +10,35 @@ $(document).ready(function() {
 	}, 4500);
 
 	//Ajax Form Send START
-	$('#contact-form').on('submit', function() { 
-		var th = $(this);
-		$.ajax({
-			type: 'POST',
-			url: 'mail.php',
-			data: th.serialize(),
-			success: function() {
-				th.trigger('reset');
-				$('.input-field').removeClass('is-active');
-				$.magnificPopup.open({
-					items: {
-						src: '<div class="form-alert"><p>Your application has been successfully sent. <br> Expect a call!</p></div>',
-						type: 'inline'
-					}
-				});
-			},
-			error: function() {
-				th.trigger('reset');
-				$('.input-field').removeClass('is-active');
-				$.magnificPopup.open({
-					items: {
-						src: '<div class="form-alert"><p>An error occurred, please try again</p></div>',
-						type: 'inline'
-					}
-				});
-			}
-		});
-		return false;
-	});
+	// $('#contact-form').on('submit', function() { 
+	// 	var th = $(this);
+	// 	$.ajax({
+	// 		type: 'POST',
+	// 		url: 'mail.php',
+	// 		data: th.serialize(),
+	// 		success: function() {
+	// 			th.trigger('reset');
+	// 			$('.input-field').removeClass('is-active');
+	// 			$.magnificPopup.open({
+	// 				items: {
+	// 					src: '<div class="form-alert"><p>Your application has been successfully sent. <br> Expect a call!</p></div>',
+	// 					type: 'inline'
+	// 				}
+	// 			});
+	// 		},
+	// 		error: function() {
+	// 			th.trigger('reset');
+	// 			$('.input-field').removeClass('is-active');
+	// 			$.magnificPopup.open({
+	// 				items: {
+	// 					src: '<div class="form-alert"><p>An error occurred, please try again</p></div>',
+	// 					type: 'inline'
+	// 				}
+	// 			});
+	// 		}
+	// 	});
+	// 	return false;
+	// });
 	//Ajax Form Send END
 
 	// Banner START
